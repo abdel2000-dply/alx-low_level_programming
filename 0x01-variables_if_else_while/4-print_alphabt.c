0b64cd6778a3ee747dfaddbc554c227eacb63b9e
abdel2000-dply <abno870@gmail.com>
2023-03-17 13:27:50 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		if (l == 'g' || l == 'e')
+		{
+			l++;
+			continue;
+		}
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
