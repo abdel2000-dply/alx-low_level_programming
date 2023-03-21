2fd28d158eb2d1d7731bd0921db398e8b0a31613
abdel2000-dply <abno870@gmail.com>
2023-03-21 14:52:17 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
