bfed31f52144c7984757c6e8fbac6add07a4f21c
abdel2000-dply <abno870@gmail.com>
2023-03-21 16:19:05 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit
+ * @n: input number as an integer
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
