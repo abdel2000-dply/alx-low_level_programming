ed06507bbd6d015d76507cacdf890809f288912d
abdel2000-dply <abno870@gmail.com>
2023-03-21 15:00:19 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i <= 10; i++)
 	{
-		print_alphabet();
+		for (j = 97 ; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
 }
