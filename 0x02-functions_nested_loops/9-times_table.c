57f3679118c0ff2fe07df5663fba136a59a80f9d
abdel2000-dply <abno870@gmail.com>
2023-03-21 18:09:09 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,17 +16,20 @@ void times_table(void)
 			res = n * i;
 			if (res >= 10)
 			{
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
 			}
 			else
 			{
+				if (i != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar(res + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
