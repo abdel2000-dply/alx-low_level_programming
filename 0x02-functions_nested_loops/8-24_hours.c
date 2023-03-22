b07a41565a282e80edcf7e4622732d4464b3a40a
abdel2000-dply <abno870@gmail.com>
2023-03-21 17:00:38 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,14 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
+					if ( a >= 2 && b >= 4)
+						break;
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(':');
