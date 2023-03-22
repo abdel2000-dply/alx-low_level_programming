017d33850d07d7111d7a6fee73f66c9f9c8744bf
abdel2000-dply <abno870@gmail.com>
2023-03-21 19:26:30 -0700

    tasl 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,33 +11,25 @@ void print_times_table(int n)
 	int res;
 
 	if (n > 15 || n < 0)
-	{
-	}
 	else
-	{
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			res = i * j;
 			if (res >= 10 && res < 100)
-			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((res / 10) + '0');
 				_putchar((res % 10) + '0');
-			}
 			else if (res >= 100)
-			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar((res / 100) + '0');
 				_putchar(((res / 10) % 10) + '0');
 				_putchar(((res % 10) % 10) + '0');
-			}
 			else
-			{
 				if (j != 0)
 				{
 					_putchar(',');
@@ -46,10 +38,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 				_putchar(res + '0');
-			}
 		}
 		_putchar('\n');
-	}
-	}
-}
+	}}
 
