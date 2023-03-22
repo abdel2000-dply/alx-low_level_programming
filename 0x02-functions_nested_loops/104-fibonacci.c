57ac72acfce709107d98da94ee60dd4c7ce6014b
abdel2000-dply <abno870@gmail.com>
2023-03-22 16:44:05 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,21 +6,30 @@
  */
 int main(void)
 {
-	unsigned long n1, n2, nextNum, i;
+	unsigned long n1, n2, nextNum, i, next1, next2;
 
 	n1 = 1, n2 = 2;
 	printf("%lu, %lu, ", n1, n2);
 	for (i = 3; i <= 98; i++)
 	{
+	if (i <= 92)
+	{
 		nextNum = n1 + n2;
 		printf("%lu", nextNum);
-		if (i != 50)
-		{
-			printf(", ");
-		}
+		printf(", ");
 		/* updating the value of n1 and n2 */
 		n1 = n2;
 		n2 = nextNum;
+	}
+	else
+	{
+		next1 = (n1 + n2) / 10000000000;
+		next2 = (n1 + n2) % 10000000000;
+		printf("%lu%lu", next1, next2);
+		printf(", ");
+		n1 = n2;
+		n2 = (next1 * 10000000000) + next2;
+	}
 	}
 	printf("\n");
 	return (0);
