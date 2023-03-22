2e6c1df6064501edd7a5a9266b24129d12327f45
abdel2000-dply <abno870@gmail.com>
2023-03-22 14:20:38 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%d\n", sum);
+}
