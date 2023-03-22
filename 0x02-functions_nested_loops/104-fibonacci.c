50ca5078787c7252328c2d1de29dab56708a714f
abdel2000-dply <abno870@gmail.com>
2023-03-22 15:32:15 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long n1, n2, nextNum, i;
+
+	n1 = 1, n2 = 2;
+	printf("%lu, %lu, ", n1, n2);
+	for (i = 3; i <= 98; i++)
+	{
+		nextNum = n1 + n2;
+		printf("%lu", nextNum);
+		if (i != 50)
+		{
+			printf(", ");
+		}
+		/* updating the value of n1 and n2 */
+		n1 = n2;
+		n2 = nextNum;
+	}
+	printf("\n");
+	return (0);
+}
