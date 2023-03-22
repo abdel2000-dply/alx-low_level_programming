0d6531d26e24616e36220b4d3422071119a5a2e5
abdel2000-dply <abno870@gmail.com>
2023-03-21 19:07:39 -0700

    tasl 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table
+ * @n: inputs as an integer
+ *
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+	int i, j;
+	int res;
+
+	if (n > 15 || n < 0)
+	{
+		break;
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			res = i * j;
+			if (res >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else
+			{
+				if (res != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(res + '0');
+			}
+		}
+		_putchar('\n');
+	}
+	}
+}
+
