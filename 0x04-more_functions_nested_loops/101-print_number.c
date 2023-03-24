94251a17ae0ce7b5d7f89fc677960b5f11666285
abdel2000-dply <abno870@gmail.com>
2023-03-23 19:41:18 -0700

    task finaaal

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: the number
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		print_number(n);
+	}
+	else if (n < 10)
+	{
+		_putchar(n + 48);
+	}
+	else if (n >= 10)
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+}
