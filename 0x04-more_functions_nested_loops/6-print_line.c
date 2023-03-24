876db267d3ad9735afc8f1a89f4f0bbde0364032
abdel2000-dply <abno870@gmail.com>
2023-03-23 17:45:48 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: integer
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
