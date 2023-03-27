473552875488e940be8bb6d4afb355bd604c84ed
abdel2000-dply <abno870@gmail.com>
2023-03-27 14:33:09 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string
+ * @s: input
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	return (i);
+}
