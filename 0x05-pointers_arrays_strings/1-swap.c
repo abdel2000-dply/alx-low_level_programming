959dcc93e5a9ea77407ec8e8904587579b8f19fd
abdel2000-dply <abno870@gmail.com>
2023-03-27 13:15:19 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: input
+ * @b: input
+ *
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int t = *a;
+	*a = b;
+	*b = t;
+}
