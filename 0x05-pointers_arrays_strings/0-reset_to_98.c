7b1fb88417b5d2657424e17215c8d8c94a91b8a5
abdel2000-dply <abno870@gmail.com>
2023-03-27 12:17:47 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - updating a value
+ * @n: a pointer the integer we want to set toa pointer the integer we want to set to
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+getting started with pointes, arrays, and strings.
