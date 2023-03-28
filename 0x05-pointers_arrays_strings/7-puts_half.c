b901b179145b107bf928628dd031ac605ef915c9
abdel2000-dply <abno870@gmail.com>
2023-03-27 17:59:30 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: input
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i, n;
+
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+		for (i = n; *str != '\0'; i++)
+		{
+			_putchar(*str);
+		}
+	}
+	else
+	{
+		n = (len - 1) / 2;
+		for (i = n; *str != '\0'; i++)
+		{
+			_putchar(*str);
+		}
+	}
+}
