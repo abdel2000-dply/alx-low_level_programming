ac9a12e5986222a3423723ab4db1489c8bb7f3b8
abdel2000-dply <abno870@gmail.com>
2023-03-27 19:07:20 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: input
+ *
+ * Return: number
+ */
+int _atoi(char *s)
+{
+	int sign = 1, res = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign = -1;
+		else if (*s >= '0' && *s <= '9')
+			res = res * 10 + (*s - '0');
+		else if (res > 0)
+			break;
+		s++;
+	}
+	return (sign * res);
+}
