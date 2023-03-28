518741aca9e2716e36d91208d63425025494cdb4
abdel2000-dply <abno870@gmail.com>
2023-03-27 17:40:20 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,13 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		_putchar(*str);
-		str = str + 2;
+		if (i % 2 == 0)
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
