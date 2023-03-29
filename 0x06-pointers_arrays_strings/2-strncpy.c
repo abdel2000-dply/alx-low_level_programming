ed9215f63b18a9729440954b90d0f8defa117fc4
abdel2000-dply <abno870@gmail.com>
2023-03-29 11:25:24 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * strncpy -  copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: size
+ *
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+	dest[i] = '\0';
+	}
+
+	return (dest);
+}
+
