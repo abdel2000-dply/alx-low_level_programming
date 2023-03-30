f4f3b04b0404515229587ef95ad5102ebc2383fd
abdel2000-dply <abno870@gmail.com>
2023-03-30 11:19:12 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	int i, dig;
+	int div = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	/* counting div ( number of digits ) */
+	i = n;
+	while (i > 9)
+	{
+		div *= 10;
+		i /= 10;
+	}
+	/* printing */
+	while (div > 0)
+	{
+		dig = (n / div) % 10;
+		_putchar(dig + '0');
+		div /= 10;
+	}
+}
