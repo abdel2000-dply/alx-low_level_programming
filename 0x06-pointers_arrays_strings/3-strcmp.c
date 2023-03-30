a4f11697e4b02b8c1c1708baf5a3fa92df45e6b5
abdel2000-dply <abno870@gmail.com>
2023-03-30 05:46:07 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,23 +9,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		i++;
+		if (s1[i] < s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-
-	if (s1[i] == s2[i])
-	{
-		return (0);
-	}
-	else if (s1[i] < s2[i])
-	{
-		return (-15);
-	}
-	else
-	{
-		return (15);
-	}
+	return (0);
 }
