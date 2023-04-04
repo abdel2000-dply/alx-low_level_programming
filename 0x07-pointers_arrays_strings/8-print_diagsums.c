b6ed462d100154462c380943f4aed6befac25c91
abdel2000-dply <abno870@gmail.com>
2023-04-03 19:24:42 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
-		sum2 += s[i * (size - 1 - i)];
+		sum2 += a[i * (size - 1 - i)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
