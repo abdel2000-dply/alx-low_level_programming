0d16448d6c4df334163a1bc5efbd75a6267a3d2c
abdel2000-dply <abno870@gmail.com>
2023-04-05 19:03:27 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -40,7 +40,7 @@ int is_palindrome_helper(char *s, int len)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
+		return (0);
 
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
