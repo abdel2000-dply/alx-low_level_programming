554df3035a78e60903fec561868a4ac15cafa735
abdel2000-dply <abno870@gmail.com>
2023-04-12 11:26:29 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,54 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * count_words - counts the number of words in a string
+ * @str: string to count words in
+ * Return: number of words in string
+ */
+int count_words(char *str)
+{
+	int count = 0, i = 0;
 
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ')
+		{
+			count++;
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (count);
+}
+/**
+ * word_len - gets the length of a word in a string
+ * @str: string containing word
+ * Return: length of word
+ */
+int word_len(char *str)
+{
+	int len = 0;
+
+	while (str[len] != ' ' && str[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * free_array - frees memory allocated to an array of strings
+ * @arr: array of strings to free
+ * @size: size of array
+ */
+void free_array(char **arr, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		free(arr[i]);
+}
 /**
  * strtow - splits a string into words
  * @str: string to split
@@ -24,11 +72,8 @@ char **strtow(char *str)
 	{
 		while (str[j] == ' ')
 			j++;
-
 		len_word = word_len(&str[j]);
-
 		words[i] = malloc((len_word + 1) * sizeof(char));
-
 		if (words[i] == NULL)
 		{
 			free_array(words, i);
@@ -37,7 +82,6 @@ char **strtow(char *str)
 		}
 		for (k = 0; k < len_word; k++)
 			words[i][k] = str[j + k];
-
 		words[i][len_word] = '\0';
 		j += len_word;
 	}
--- 0x0B-malloc_free/library1.h
+++
@@ -1,3 +0,0 @@
-int count_words(char *str);
-int word_len(char *str);
-void free_array(char **arr, int size);
