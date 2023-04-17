57c0310cd2d46b6bbe79c531b0714ba738417382
abdel2000-dply <abno870@gmail.com>
2023-04-17 15:30:06 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog 
+/**
+ * struct dog - contains info about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+struct dog
 {
 	char *name;
 	float age;
