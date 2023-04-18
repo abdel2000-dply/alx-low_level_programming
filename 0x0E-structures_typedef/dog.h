630c9903de303a3a14218ed18c9ac218178ee848
abdel2000-dply <abno870@gmail.com>
2023-04-17 18:09:34 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,10 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - structure
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
