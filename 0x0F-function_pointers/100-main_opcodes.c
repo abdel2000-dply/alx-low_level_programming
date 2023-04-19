4c59c19a2af3512043213c88da32e4636fead2b4
abdel2000-dply <abno870@gmail.com>
2023-04-19 15:41:41 -0700

    task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,15 +14,15 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 	num_bytes = atoi(argv[1]);
 	if (num_bytes <= 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
-	start = (char *) &main;
+	start = (char *)main;
 	for (i = 0; i < num_bytes; i++)
 	{
 		printf("%02x ", start[i] & 0xff);
