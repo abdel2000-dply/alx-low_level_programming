f0a51804da951d40c3669b1ad3fb857d3b82c38f
abdel2000-dply <abno870@gmail.com>
2023-04-20 14:52:05 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,8 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
 	{
