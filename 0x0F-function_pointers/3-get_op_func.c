c7e9af4df2ec2e85f54e5872c4601cf7e5e042ff
abdel2000-dply <abno870@gmail.com>
2023-04-19 17:34:02 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,7 +24,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (*ops[i].op == *s && *(s + 1) == '\0')
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
