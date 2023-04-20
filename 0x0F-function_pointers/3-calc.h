c69e6d9c761a0c6e215e47bf6aaa545ce7e6ed45
abdel2000-dply <abno870@gmail.com>
2023-04-19 17:30:25 -0700

    task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 /**
@@ -20,6 +21,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
+
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,8 +22,12 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
