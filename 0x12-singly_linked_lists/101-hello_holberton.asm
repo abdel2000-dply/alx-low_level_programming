3f77b55908f86350c608174e955ef226d9b61968
abdel2000-dply <abno870@gmail.com>
2023-04-27 16:51:44 -0700

    task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,18 @@
 section .data
-	msg db 'hello, holberton', 10
-section .text
-	global main
-	extern printf
-main:
-	mov rdi, msg
-	xor eax, eax
-	call printf
+    msg db 'Hello, Holberton', 10, 0
 
-	ret
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rdi, msg
+    xor eax, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    ret
