a0260338f2c5b48ed1961472be648898ee7b7874
abdel2000-dply <abno870@gmail.com>
2023-05-01 10:16:50 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: double pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+
+	while (*head)
+	{
+		curr = *head;
+		*head = (*head)->next;
+		free(curr);
+	}
+
+	*head = NULL;
+}
