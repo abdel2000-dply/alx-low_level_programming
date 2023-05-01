b12c26296ca71bd847ffb158c9b180338dfdbcea
abdel2000-dply <abno870@gmail.com>
2023-05-01 15:50:52 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  *
  * Return: the nth node of the list.
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
