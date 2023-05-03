97261a3f7ada2b1317ca727008340c0526739796
abdel2000-dply <abno870@gmail.com>
2023-05-02 18:52:52 -0700

    a quick update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,7 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 		i++;
 		tmp = curr->next;
 
-		if (tmp >= curr)
+		if (tmp > curr)
 		{
 			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
 			break;
