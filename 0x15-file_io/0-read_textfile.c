ad816b91fa19beb771da9f08aa85d16428df43fa
abdel2000-dply <abno870@gmail.com>
2023-05-08 17:08:10 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX.
+ * @filename: the name of the file.
+ * @letters: number of letters it should read and print.
+ *
+ * Return: the actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fpt;
+	ssize_t nr, nw;
+	char *b = malloc(sizeof(char) * letters);
+
+	if (!filename)
+		return (0);
+
+	if(!b)
+		return (0);
+
+	fpt = open(filename, 0_RDONLY);
+	if (fpt == -1)
+	{
+		free(b);
+		return (0);
+	}
+
+	nr = read(fpt, b, letters);
+	if (nr == -1)
+	{
+		free(b);
+		close(fpt);
+		return (0);
+	}
+
+	nw = write(STDOUT_FTLENO, b, nr);
+	if (nw == -1 || nw != nr)
+	{
+		free(b);
+		close(fpt);
+		return (0);
+	}
+
+	free(b);
+	close(fpt);
+	return (nw);
+}
