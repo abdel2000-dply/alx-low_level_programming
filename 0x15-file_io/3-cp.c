d296e8762cade97d189bf99e89a6ba63fcf71796
abdel2000-dply <abno870@gmail.com>
2023-05-08 18:11:48 -0700

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 97-100 on error
+ */
+int main(int argc, char **argv)
+{
+
+}
