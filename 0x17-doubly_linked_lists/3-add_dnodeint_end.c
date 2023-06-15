b1ce6e685baa5e2bb0552a738f0c9c1fca3c68fd
abdel2000-dply <abno870@gmail.com>
2023-06-15 22:11:16 +0100

    task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,7 +20,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (*head == NULL) {
+	if (*head == NULL)
+	{
 		new->prev = NULL;
 		*head = new;
 		return (new);
