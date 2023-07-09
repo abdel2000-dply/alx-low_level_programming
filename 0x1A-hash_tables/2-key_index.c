089f3f0d418033708e2a880b8ff34ef352e363a3
abdel2000-dply <abno870@gmail.com>
2023-07-09 18:08:11 +0100

    task 2 and 3

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,53 +1,19 @@
 #include <stdlib.h>
-#include <string.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds or updates an element in the hash table
- * @ht: The hash table to add/update the element to
- * @key: The key to add/update
- * @value: The value associated with the key
- * Return: 1 if it succeeded, 0 otherwise
+ * key_index - Gives the index of a key in the hash table array
+ * @key: The key
+ * @size: The size of the array of the hash table
+ *
+ * Return: The index at which the key/value pair should be stored
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *temp_node;
+	unsigned long int hash_value, index;
 
-	if (!ht || !key || *key == '\0')
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	temp_node = ht->array[index];
-	while (temp_node != NULL)
-	{
-		if (strcmp(temp_node->key, key) == 0)
-		{
-			free(temp_node->value);
-			temp_node->value = strdup(value);
-			if (temp_node->value == NULL)
-				return (0);
-			return (1);
-		}
-		temp_node = temp_node->next;
-	}
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
 
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
+	return (index);
 }
