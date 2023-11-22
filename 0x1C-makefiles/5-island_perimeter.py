2fbb01089af81008260fc05bb10385c7886d5649
abdel2000-dply <abno870@gmail.com>
2023-11-22 22:46:46 +0100

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Function to calculate the perimeter of the island in the grid.
+    """
+    rows = len(grid)
+    cols = len(grid[0]) if rows else 0
+
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
